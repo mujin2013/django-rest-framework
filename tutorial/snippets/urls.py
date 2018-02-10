ec81c27bd5f68f8c3d31300a20527328cde6a196
mujin2013 <2814241400@qq.com>
2018-02-10 14:14:24 +0800

    教程1： Serialization
    目前为止，我们做得还行，我们做的序列化API感觉跟Django的Form API 比较相似，并且我们做了一些普通的Django视图。
    
    我们的API视图，现在还没做啥特别的事情。除了响应了json之外，还有一些没能处理的边缘事件，但至少还是个能用的Web API。
    
    我们会在教程的第二部分(tutorial part 3)，来看看如何改进。

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,10 @@
+#i!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+
+from django.conf.urls import url
+from snippets import views
+
+urlpatterns = [
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+]
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+#i!/usr/bin/env python
+# -*- coding:utf-8 -*- 
 
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+@csrf_exempt    #因为我们需要POST数据，到这个视图的客户端，并没有CSRF令牌（token），所以我们需要为该视图标记为 csrf_exempt 
+def snippet_list(request):
+    """
+    @description: 列出所有的代码片段或创建一个新的代码片段
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)    #将querySet进行序列化
+        return JsonResponse(serializer.data, safe=False)    #safe若设置为False,那么第一个参数可以是任何可被JSON序列化的对象（https://docs.djangoproject.com/en/2.0/ref/request-response/#jsonresponse-objects）
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)    #将request解析成python的原生数据类型
+        serializer = SnippetSerializer(data=data)    #将原生数据类型，还原到一个被填充完毕的对象实例中
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    @description: 获取、更新、删除一个代码片段
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)    #对模型实例进行序列化
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
+
+
+
