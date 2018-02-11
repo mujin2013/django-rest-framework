da58df2078e2b5497ff1d0a5e100d4066446eb6e
mujin2013 <2814241400@qq.com>
2018-02-11 16:45:09 +0800

    教程6：ViewSets和Routers
    使用ViewSets和Routers实现自动生成路由（路由不需要人为配置了，它会自动生成）

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,25 +2,17 @@
 # -*- coding:utf-8 -*- 
 
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
-# urlpatterns = [
-#     url(r'^$', views.api_root),
-#     url(r'^users/$', views.UserList.as_view()),
-#     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-#     url(r'^snippets/$', views.SnippetList.as_view()),
-#     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-#     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
-# ]
 
-# urlpatterns = format_suffix_patterns(urlpatterns)
+#创建路由器并将我们的viewset注册
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
-urlpatterns = format_suffix_patterns([
-        url(r'^$', views.api_root),
-        url(r'^snippets$', views.SnippetList.as_view(), name='snippet-list'),
-        url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
-        url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-        url(r'^users/$', views.UserList.as_view(), name='user-list'),
-        url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail')
-    ])
+#API的urls由我们的router自动确定
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
+
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,47 +6,38 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
 from rest_framework import permissions
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework import viewsets
 from django.contrib.auth.models import User
 
 
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    @description: viewset会自动提供list和detail操作
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    @description: viewset会自动提供list、create、retrieve、update、destory操作
+                我们也会额外提供highlight操作
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                            IsOwnerOrReadOnly,)
 
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
 
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-            'users': reverse('user-list', request=request, format=format),
-            'snippets': reverse('snippet-list', request=request, format=format)
-        })
